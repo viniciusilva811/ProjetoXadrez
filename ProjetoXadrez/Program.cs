6c26cac583ba465e0bcde9c01580540dcbdb2d78
Vinícius Silva <vinisilva45@live.com>
2020-09-09 16:48:47 -0300

    Adicionar arquivos de projeto.

---
+++ ProjetoXadrez/Program.cs
@@ -0,0 +1,20 @@
+﻿using ProjetoXadrez.Tabuleiro;
+using System;
+
+namespace ProjetoXadrez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao posi;
+
+            posi = new Posicao(3, 4);
+
+            Console.WriteLine("Posicao: " + posi);
+
+            Console.ReadLine();
+
+        }
+    }
+}
